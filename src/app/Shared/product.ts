3e387d942991b220c048d070d5612fd2fde7d817
prasanthgpks <prasanthgpks@gmail.com>
2018-07-22 19:30:56 +1000

    Initial commit

---
+++ src/app/Shared/product.ts
@@ -0,0 +1,12 @@
+export interface IProduct{
+
+    productId : Number;
+    productName : string;
+    productCode : string;
+    releaseDate : string;
+    description : string
+    price: number;
+    starRating : number;
+    imageUrl:string;
+
+}
